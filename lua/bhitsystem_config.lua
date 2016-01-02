a33c4adae5fcf81750334fa7d03326378f39f273
Matt S <XxLMM13xXgaming@gmail.com>
2016-01-02 17:29:48 -0500

    testing

--- lua/bhitsystem_config.lua
+++ lua/bhitsystem_config.lua
@@ -3,7 +3,10 @@ BHitSysConfig.DevMode = true -- DO NOT TOUCH
 ----------------------------------------------------------
 -- Blur HitSystem Made by:
 -- Skater ()
--- XxLMM13xXgaming () I am cool
+-- XxLMM13xXgaming ()
 ----------------------------------------------------------
 
 BHitSysConfig.HitmanTeams = { "Hitman" } -- Teams that can use the hitman menu (are the hitmans)
+
+
+---------------lsdjhvlf;jvbhlxjchvkjchvckjxbhcxkjbv,nbg,jb 
