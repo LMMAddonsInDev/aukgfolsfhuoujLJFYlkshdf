debd03d3c1e2e4470130061455ae964d521f37ac
LMMAddonsInDev <gamesxtutorials@gmail.com>
2016-01-02 15:33:11 -0700

    Change 1

--- lua/bhitsystem_config.lua
+++ lua/bhitsystem_config.lua
@@ -3,7 +3,7 @@ BHitSysConfig.DevMode = true -- DO NOT TOUCH
 ----------------------------------------------------------
 -- Blur HitSystem Made by:
 -- Skater ()
--- XxLMM13xXgaming ()
+-- XxLMM13xXgaming () I am cool
 ----------------------------------------------------------
 
 BHitSysConfig.HitmanTeams = { "Hitman" } -- Teams that can use the hitman menu (are the hitmans)
